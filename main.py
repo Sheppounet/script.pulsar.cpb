7b6d752beb7e344a80b85d5600a52cc3a468da88
Sheppounet <jopub1@voila.fr>
2015-01-06 21:45:49 +0100

    0.4.0
    - Correction URI retournee suite a refonte du site
    - Optimisation REGEX de filtrage pour cibler exclusivement films ou
    series + recuperer le nom du torrent
    - Ajout du nom du torrent au payload, pour passer le nouveau filtre des
    episodes
    - Hack manuel pour certaines series dont le nom FR n'est pas reconnu sur
    CPB
    - Compatibilite Pulsar 0.4.x

--- main.py
+++ main.py
@@ -22,13 +22,16 @@ def search(query):
     query = query.replace('11111',ACTION_SERIES).replace('22222',ACTION_FILMS)
     provider.log.info("GET : %s/%s/%s.html" % (__baseUrl__, ACTION_SEARCH, query))
     resp = provider.GET("%s/%s/%s.html" % (__baseUrl__, ACTION_SEARCH, query))
-    # REGEX to find wanted links - Use the class to exclude ads "top downloaded" links
-    p = re.compile(ur'(/dl-torrent/\S*\.html).*?class="titre"')
+    # REGEX to find wanted links - 2 capturing groups
+    # - (?:films|series) : match only films or series, ?: is to exclude this group from capturing, which generate a tuple as return
+    # - Use the class to exclude ads "top downloaded" links
+    # - >(.*?)< to get the media name
+    # It brings back a tuple : torrent[0] = uri, torrent[1] = name
+    p = re.compile(ur'(/dl-torrent/(?:films|series)/\S*\.html).*?class="titre">(.*?)<')
     # Uncomment if needed to get optimal perfs
     #for torrent in re.findall(p, resp.data) :
-    #    provider.log.debug("REGEX FOUND %s" % torrent) 
-    return [{"uri": __baseUrl__ + "/_torrents/" + torrent.rpartition('/')[2].replace(".html",".torrent")} for torrent in re.findall(p, resp.data)]
-    #return [{"uri": __baseUrl__ + "/_torrents/" + torrent.rpartition('/')[2].replace(".html",".torrent")} for torrent in liens]
+    #    provider.log.debug("REGEX FOUND %s" % torrent[0]) 
+    return [{"name": torrent[1], "uri": __baseUrl__ + "/telechargement/" + torrent[0].rpartition('/')[2].replace(".html",".torrent")} for torrent in re.findall(p, resp.data)]
 
 # Episode Payload Sample
 # {
@@ -40,15 +43,18 @@ def search(query):
 # "titles": null
 # }
 def search_episode(episode):
-    provider.log.info("Search episode : name %(title)s, season %(season)02d, episode %(episode)02d" % episode)
-    response = provider.GET("%s/find/%s?api_key=%s&language=fr&external_source=imdb_id" % (tmdbUrl, episode['imdb_id'], tmdbKey))
-    provider.log.debug(response)
-    if response != (None, None):
-        name_normalize = unicodedata.normalize('NFKD',response.json()['tv_results'][0]['name'])
-        episode['title'] = ''.join(c for c in name_normalize if (not unicodedata.combining(c)))
-        provider.log.debug('FRENCH title :  %s' % episode['title'])
-    else :
-        provider.log.error('Error when calling TMDB. Use Pulsar movie data.')
+    provider.log.info("Search episode : name %(title)s, season %(season)02d, episode %(episode)02d, imdb_id %(imdb_id)s" % episode)
+    if episode['imdb_id']!= 'tt0436992' and episode['imdb_id']!='tt0944947' :
+        # Disable french title for Doctor Who 'tt0436992' (return "Dr Who") and 
+        # Game of thrones 'tt0944947' (Le trone de fer). May be some other are bad with their FR title :/
+        response = provider.GET("%s/find/%s?api_key=%s&language=fr&external_source=imdb_id" % (tmdbUrl, episode['imdb_id'], tmdbKey))
+        provider.log.debug(response)
+        if response != (None, None):
+            name_normalize = unicodedata.normalize('NFKD',response.json()['tv_results'][0]['name'])
+            episode['title'] = ''.join(c for c in name_normalize if (not unicodedata.combining(c)))
+            provider.log.debug('FRENCH title :  %s' % episode['title'])
+        else :
+            provider.log.error('Error when calling TMDB. Use Pulsar movie data.')
     return search("11111%(title)s S%(season)02dE%(episode)02d" % episode)
 
 # Movie Payload Sample
